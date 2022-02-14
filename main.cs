7ea3c4a1f5ac3a7d66887956b9fbf54325a49451
Ronnie Goodrich <kahootbird@gmail.com>
2022-02-14 16:10:10 -0500

    reading price

--- main.cs
+++ main.cs
@@ -1,9 +1,31 @@
-using System; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
 
 public class Program {
 
   public static void Main(string[] args) {
-    Console.WriteLine("hello world");
+    /*
+    XmlDocument Doc = new XmlDocument();
+    Doc.Load("booklist.xml");
+    Doc.Save(Console.Out);
+    */
+    XmlTextReader xmltxtread = new XmlTextReader("booklist.xml");
+
+      Console.WriteLine("END");
+    while (xmltxtread.Read())
+    {
+      //Console.WriteLine(xmltxtread.Name);
+      //while (xmltxtread.NodeType == XmlNodeType.Element && xmltxtread.Name == "bookstore")
+
+      if (xmltxtread.NodeType == XmlNodeType.Element && xmltxtread.Name == "price")
+      {
+        Console.WriteLine(xmltxtread.ReadElementString());
+      }
+
+    }
   }
 }
-
