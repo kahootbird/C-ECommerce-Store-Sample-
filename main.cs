a49679396939db24268acfecaf18e3f091471acd
Ronnie Goodrich <kahootbird@gmail.com>
2022-02-15 11:46:15 -0500

    Working example

--- main.cs
+++ main.cs
@@ -1,31 +1,32 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
-
+//github: https://github.com/kahootbird/C-ECommerce-Store-Sample-
+//This program reads booklist.xml and prints the book title and price then the sum of all prices
 public class Program {
 
   public static void Main(string[] args) {
-    /*
-    XmlDocument Doc = new XmlDocument();
-    Doc.Load("booklist.xml");
-    Doc.Save(Console.Out);
-    */
+    //Read booklist.xml
     XmlTextReader xmltxtread = new XmlTextReader("booklist.xml");
+    double price = 0;
+    var str = "";
 
-      Console.WriteLine("END");
+    Console.WriteLine("List of all book titles, price, and price of all books combined");
     while (xmltxtread.Read())
     {
-      //Console.WriteLine(xmltxtread.Name);
-      //while (xmltxtread.NodeType == XmlNodeType.Element && xmltxtread.Name == "bookstore")
-
-      if (xmltxtread.NodeType == XmlNodeType.Element && xmltxtread.Name == "price")
+      //Read book title then price and print each
+      if (xmltxtread.NodeType == XmlNodeType.Element && xmltxtread.Name == "title")
       {
         Console.WriteLine(xmltxtread.ReadElementString());
       }
-
+      if (xmltxtread.NodeType == XmlNodeType.Element && xmltxtread.Name == "price")
+      {
+        //Ideally use try catch on double, for this simplified example I'm avoiding it
+        str = xmltxtread.ReadElementString();
+        var price2double = Convert.ToDouble(str);
+        Console.WriteLine("$" + str);
+        price += price2double;
+      }
     }
+    Console.WriteLine("Price of all books is: " + price);
   }
 }
